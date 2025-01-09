6f91ebcb6b231d90f4d19c6b35aca773bec6e975
srforjob25 <srforjob25@gmail.com>
2025-01-10 05:18:09 +0900

    Material UIを導入

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,8 +1,13 @@
+import { Rating } from "@mui/material";
+
 function App() {
   return (
     <>
       <div className="m-5 p-3 border-2 border-gray-400">
         <h1 className="text-4xl font-extrabold text-[#37BCF8]">Hello tailwind!</h1>
+      </div>
+      <div className="mt-5">
+        <Rating />
       </div>
     </>
   );
