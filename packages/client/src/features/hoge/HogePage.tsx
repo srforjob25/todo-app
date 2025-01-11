7b5a40250641a25a4e2cc6ccc3c8586bd329c921
srforjob25 <srforjob25@gmail.com>
2025-01-11 17:03:13 +0900

    jotaiを導入

--- packages/client/src/features/hoge/HogePage.tsx
+++ packages/client/src/features/hoge/HogePage.tsx
@@ -1,16 +1,28 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { trpc } from "../../utils/trpc";
+import { countAtom } from "../../utils/jotai";
+import { useAtom } from "jotai";
 
 const HogePage: React.FC = () => {
   const response = trpc.todo.getTodos.useQuery();
 
   const todos = response.data;
+
+  const [count, setCount] = useAtom(countAtom);
+
   return (
     <Box>
       <h1 className="text-4xl font-extrabold text-[#37BCF8]">hoge</h1>
       {todos?.map((todo) => {
         return <h1 className="text-4xl font-extrabold text-[#37BCF8]">{todo.title}</h1>;
       })}
+      <h1 className="text-4xl font-extrabold text-[#37BCF8]">{count}</h1>
+      <Button className="my-3" variant="contained" onClick={() => setCount((prev) => prev + 1)}>
+        +
+      </Button>
+      <Button className="my-3" variant="contained" onClick={() => setCount((prev) => prev - 1)}>
+        -
+      </Button>
     </Box>
   );
 };
